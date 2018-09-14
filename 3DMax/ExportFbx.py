2fbb7d0e4f5b01988d8557ca8f105c91b30fa969
unknown <liuhongyi@snda.com>
2018-09-14 17:43:13 +0800

    导出fbx

--- 3DMax/ExportFbx.py
+++ 3DMax/ExportFbx.py
@@ -1,4 +1,4 @@
-import sys
+import sys,os,math
 sys.path.append("D:\\Program Files\\Autodesk\\3ds Max 2016")
 import MaxPlus
 
@@ -7,9 +7,11 @@ def DealWithNodes():
     root = MaxPlus.Core.GetRootNode()
 
     nodelist = []  
-
+    dellist = []
     for node in root.Children:
-        if not 'UCX_' in node.Name:
+        if '(Dummy)' in str(node.Object):
+            dellist.append(node)
+        elif not 'UCX_' in node.Name:
             parent = node
         else:
             nodelist.append(node)
@@ -17,15 +19,51 @@ def DealWithNodes():
     for child in nodelist:
         parent.AttachChild(child)
 
-def main():
-    openfile = "D:\\Public_Ship_04.max"
-    savefile = "E:\\Public_Ship_04.max"
-    exportfile = "E:\\Public_Ship_04.fbx"
-    MaxPlus.FileManager.Open(openfile,True,False,False)
+    for delchild in dellist:
+        delchild.Delete()
 
-    DealWithNodes()
-    MaxPlus.FileManager.Export(exportfile,True)
-    MaxPlus.FileManager.Save(savefile)
+def RotatePivot():
+    root = MaxPlus.Core.GetRootNode()
+    allnodes = Descendants(root)
+    for node in allnodes:
+        node.SetPivotMode(1)
+        node.Rotate(MaxPlus.Quat(-1, 0, 0, 1),0,MaxPlus.Matrix3(1),False,False,1)
+
+def Descendants(node):
+    for c in node.Children:
+        yield c
+        for d in Descendants(c):
+            yield d
+
+def main():
+    opendir = "D:\\Scr"
+    savedir = "D:\\Des"
+    if not os.path.isdir(opendir) or not os.path.isdir(savedir):
+        return
+    
+    filelist = os.listdir(opendir)
+    for onefile in filelist:
+        ext = os.path.splitext(onefile)[1].lower()
+        if ext == ".max":
+            print(onefile)
+            openfile = os.path.join(opendir,onefile)
+            savefile = os.path.join(savedir,onefile)
+            exportfile = os.path.join(savedir,onefile).replace('.max','.fbx')
+            MaxPlus.FileManager.Open(openfile,True,False,False)
+            RotatePivot()
+            DealWithNodes()
+            MaxPlus.FileManager.Export(exportfile,True)
+            MaxPlus.FileManager.Save(savefile)
+        if ext == ".fbx":
+            print(onefile)
+            openfile = os.path.join(opendir,onefile)
+            savefile = os.path.join(savedir,onefile)
+            exportfile = os.path.join(savedir,onefile).replace('.max','.fbx')
+            MaxPlus.FileManager.Import(openfile)
+            RotatePivot()
+            DealWithNodes()
+            MaxPlus.FileManager.Export(exportfile,True)
+            MaxPlus.FileManager.Reset(True)
 
 if __name__ == '__main__':
 	main()
